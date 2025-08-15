103daba50cfe179b3216fec315d562a273912a6e
FluffyRudy <192571480+FluffyBrudy@users.noreply.github.com>
2025-08-15 14:38:56 +0545

    add dtos for tag and user command

--- src/tag/dto/create-tag.dto.ts
+++ src/tag/dto/create-tag.dto.ts
@@ -1,1 +1,11 @@
-export class CreateTagDto {}
+import { IsNumberString, IsString, Length } from 'class-validator';
+import { Tag } from '../entities/tag.entity';
+
+export class CreateTagDto {
+  @IsNumberString()
+  id: Tag['id'];
+
+  @IsString()
+  @Length(3, 50)
+  name: string;
+}
--- src/usercommand/dto/create-usercommand.dto.ts
+++ src/usercommand/dto/create-usercommand.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUsercommandDto {}
+import { IsString, Matches } from 'class-validator';
+import { Tag } from 'src/tag/entities/tag.entity';
+
+export class CreateUsercommandDto {
+  @IsString()
+  command: string;
+
+  @IsString()
+  arguments: string;
+
+  @IsString()
+  note: typeof JSON.stringify;
+
+  @IsString({ each: true })
+  @Matches(/[a-z]+(?:-[a-z0-9]+)*/, { each: true })
+  tags: Array<Tag['name']>;
+}
