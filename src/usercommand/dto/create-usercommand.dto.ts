f77a6b124cb08dad4e762277900a06b842cb92d6
FluffyRudy <192571480+FluffyBrudy@users.noreply.github.com>
2025-08-16 18:08:45 +0545

    fix: add entire tag matching feat insted of substring

--- src/usercommand/dto/create-usercommand.dto.ts
+++ src/usercommand/dto/create-usercommand.dto.ts
@@ -1,5 +1,4 @@
 import { IsObject, IsString, Matches } from 'class-validator';
-import { Tag } from 'src/tag/entities/tag.entity';
 
 export class CreateUsercommandDto {
   @IsString()
@@ -12,6 +11,10 @@ export class CreateUsercommandDto {
   note: object;
 
   @IsString({ each: true })
-  @Matches(/[a-z]+(?:-[a-z0-9]+)*/, { each: true })
-  tags: Array<Tag['name']>;
+  @Matches(/^[a-z]+(?:-[a-z0-9]+)*$/, {
+    each: true,
+    message:
+      'Each tag must be lowercase, start with a letter, and only contain letters, numbers, or hyphens (e.g. "work-project").',
+  })
+  tags: string[];
 }
