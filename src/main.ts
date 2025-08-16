123e9d49cc02870a62024247b45d0f422dc79911
FluffyRudy <192571480+FluffyBrudy@users.noreply.github.com>
2025-08-16 14:18:32 +0545

    integrate swagger ui documentation

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,27 @@ import { ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(cookieParser(process.env.COOKIE_SECRET as string));
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 void bootstrap();
