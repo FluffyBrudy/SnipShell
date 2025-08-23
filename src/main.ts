b72b1468eee4ab2839c9a148b7779d5d391c022e
rudy <fluffyrudy@proton.me>
2025-08-23 21:00:46 +0545

    remove explicit export

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,4 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 
-if (process.env.NODE_ENV === 'dev') {
-  void bootstrap();
-}
-export default bootstrap;
+bootstrap();
